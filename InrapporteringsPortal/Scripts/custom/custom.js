08024245709662242e13bb5597237680d3fb6bac
Marie Åhlin <marie.ahlin@consid.se>
2017-12-15 12:10:33 +0100

    Databasfix efter dator-krasch + Scrollhantering.

--- InrapporteringsPortal/Scripts/custom/custom.js
+++ InrapporteringsPortal/Scripts/custom/custom.js
@@ -1,6 +1,12 @@
-﻿//$(document).ready(function() {
-//    alert("Ready");
-//});
+﻿$(document).ready(function() {
+    //alert("Ready");
+
+    $(window).load(function () {
+        //alert("load");
+        //setFixedTableHeader();
+    });
+
+});
 
 
 
@@ -13,4 +19,30 @@
 //        $headerStopScroll.css({ 'position': 'relative', 'top': 'auto' });
 //    }
 //});
+
+function setFixedTableHeader() {
+    var $body = $('.contentBody');
+    //alert("setFixed");
+    $(window).scroll(function () {
+        //alert($(window).scrollTop());
+        if ($.browser.msie) {
+            alert("IE");
+            if ($(window).scrollTop() > 40) {
+                height = 196;
+                $body.css({ 'position': 'fixed', 'top': height + 'px' });
+            } else {
+                $body.css({ 'position': 'relative', 'top': 'auto' });
+            }
+        } else {
+            alert("Firefox");
+            if ($(window).scrollTop() > 80) {
+                height = 196;
+
+                $body.css({ 'position': 'fixed', 'top': height + 'px' });
+            } else {
+                $body.css({ 'position': 'relative', 'top': 'auto' });
+            }
+        }
+    });
+}
 
