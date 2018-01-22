f77409d36968a5d7cc9ba90427ba5d6dafd13aef
Marie Åhlin <marie.ahlin@consid.se>
2018-01-22 12:41:08 +0100

    .

--- InrapporteringsPortal/Controllers/HomeController.cs
+++ InrapporteringsPortal/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Inrapporteringsportal.DataAccess.Repositories;
 using InrapporteringsPortal.ApplicationService;
+using InrapporteringsPortal.ApplicationService.Helpers;
 using InrapporteringsPortal.ApplicationService.Interface;
 using InrapporteringsPortal.DataAccess;
+using InrapporteringsPortal.Web.Models;
 
 namespace InrapporteringsPortal.Web.Controllers
 {
@@ -16,8 +19,23 @@ namespace InrapporteringsPortal.Web.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.Text = _portalService.HamtaInformationsText("Startsida");
-            return View();
+            try
+            {
+                ViewBag.Text = _portalService.HamtaInformationsText("Startsida");
+                return View();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                ErrorManager.WriteToErrorLog("HomeController", "Index", e.ToString(), e.HResult);
+                var errorModel = new CustomErrorPageModel
+                {
+                    Information = "Ett fel inträffade vid öppning av startsidan.",
+                    ContactEmail = ConfigurationManager.AppSettings["ContactEmail"],
+                    ContactPhonenumber = ConfigurationManager.AppSettings["ContactPhonenumber"]
+                };
+                return View("CustomError", errorModel);
+            }
         }
 
         public ActionResult About()
