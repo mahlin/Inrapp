fbbc0314a0ef61d495bd531daeceaf8f6373e647
Marie Åhlin <marie.ahlin@consid.se>
2018-02-05 14:45:08 +0100

    Öppet/stängt-tider => olika sidor

--- InrapporteringsPortal/Controllers/HomeController.cs
+++ InrapporteringsPortal/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 using Inrapporteringsportal.DataAccess.Repositories;
 using InrapporteringsPortal.ApplicationService;
@@ -21,8 +22,18 @@ namespace InrapporteringsPortal.Web.Controllers
         {
             try
             {
-                ViewBag.Text = _portalService.HamtaInformationsText("Startsida");
-                return View();
+                //Kolla om öppet, annars visa stängt-sida
+                if (IsOpen())
+                {
+                    ViewBag.Text = _portalService.HamtaInformationsText("Startsida");
+                    return View();
+                }
+                else
+                {
+                    ViewBag.Text = _portalService.HamtaInformationsText("Stangtsida");
+                    return View("Closed");
+                }
+
             }
             catch (Exception e)
             {
@@ -64,6 +75,75 @@ namespace InrapporteringsPortal.Web.Controllers
             ViewBag.Message = "Kontakta oss";
 
             return View();
+        }
+
+        private bool IsOpen()
+        {
+            var open = true;
+
+            var now = DateTime.Now;
+            var today = now.DayOfWeek.ToString();
+            var hourNow = now.Hour;
+            var minuteNow = now.Minute;
+
+            //var closedDays = WebConfigurationManager.AppSettings["ClosedDays"];
+            var closedDays = new List<string>(WebConfigurationManager.AppSettings["ClosedDays"].Split(new char[] { ',' }));
+
+            var closedFromHour = Convert.ToInt32(WebConfigurationManager.AppSettings["ClosedFromHour"]);
+            var closedFromMin = Convert.ToInt32(WebConfigurationManager.AppSettings["ClosedFromMin"]);
+            var closedToHour = Convert.ToInt32(WebConfigurationManager.AppSettings["ClosedToHour"]);
+            var closedToMin = Convert.ToInt32(WebConfigurationManager.AppSettings["ClosedToMin"]);
+            var closedAnyway = Convert.ToBoolean(WebConfigurationManager.AppSettings["ClosedAnyway"]);
+
+
+            //Test
+            //hourNow = 8;
+            //minuteNow = 2;
+            
+            if (closedAnyway)
+            {
+                return false;
+            }
+
+            //Check day
+            foreach (var day in closedDays)
+            {
+                if (day == today)
+                {
+                    return false;
+                }
+            }
+
+            //Check time
+            //if ((closedFromHour > hourNow) && (hourNow > closedToHour))
+            //{
+            //    return true;
+            //}
+
+            //After closing
+            if ((closedFromHour <= hourNow))
+            {
+                //Check minute
+                if (minuteNow < closedFromMin)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            //Before opening?
+            if ((closedToHour <= hourNow))
+            {
+                //Check minute
+                if (minuteNow > closedToMin)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+
+            return true;
         }
     }
 }
