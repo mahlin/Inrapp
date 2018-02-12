96378803ae65f3b386281b2540215d8cb772845b
Marie Åhlin <marie.ahlin@consid.se>
2018-02-12 15:28:05 +0100

    Gränssnittsfix

--- InrapporteringsPortal/App_Start/BundleConfig.cs
+++ InrapporteringsPortal/App_Start/BundleConfig.cs
@@ -11,10 +11,6 @@ namespace InrapporteringsPortal.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/globalize").Include(
-            //    "~/Scripts/globalize.js",
-            //    "~/Scripts/message.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                 "~/Scripts/jquery-ui-{version}.js"));
 
